c91ef13c5ac7dae30954741705772a9c3be315d1
Nathan Swift <nathan.swift@swiftstrategies.co.uk>
2010-06-04 00:04:45 +0100

    Added title tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  integrate_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_tag("title","Ruby on Rails Tutorial App | Home")
     end
   end
 
@@ -14,12 +20,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_tag("title","Ruby on Rails Tutorial App | contact")
+    end
   end
   
   describe "Get 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_tag("title","Ruby on Rails Tutorial App | About")
     end
   end
   
