447568bb7a461b82d63f686efaffb90b58a65ebc
Nathan Swift <nathan.swift@swiftstrategies.co.uk>
2010-06-04 15:18:11 +0100

    Added Help page and tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe PagesController do
   integrate_views
 
+  before(:each) do 
+  # 
+   @base_title = "Ruby on Rails Tutorial App | " 
+  # 
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,7 +17,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_tag("title","Ruby on Rails Tutorial App | Home")
+      response.should have_tag("title", @base_title + "Home")
     end
   end
 
@@ -23,7 +29,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'contact'
-      response.should have_tag("title","Ruby on Rails Tutorial App | Contact")
+      response.should have_tag("title", @base_title + "Contact")
     end
   end
   
@@ -35,7 +41,19 @@ describe PagesController do
     
     it "should have the right title" do
       get 'about'
-      response.should have_tag("title","Ruby on Rails Tutorial App | About")
+      response.should have_tag("title", @base_title + "About")
+    end
+  end
+
+  describe "Get 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_tag("title", @base_title + "Help")
     end
   end
   
